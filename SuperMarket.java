b2462d6be23d184ca3d0b449a5db99178f38f42d
muthuramalingam <muthu1809@gmail.com>
2021-03-24 19:26:54 +0530

    Adding SuperMarket java program

---
+++ SuperMarket.java
@@ -0,0 +1,32 @@
+public class SuperMarket
+{
+int price; 
+int discount; 
+public static void main(String[] args)
+{
+SuperMarket rice = new SuperMarket(); 
+SuperMarket wheat = new SuperMarket(); 
+SuperMarket dates = new SuperMarket(); 
+rice.price = 60; 
+rice.discount = 6; 
+wheat.price = 50;
+wheat.discount = 5; 
+dates.price = 30; 
+dates.discount = 4; 
+System.out.println(rice.price); 
+System.out.println(rice.discount);
+System.out.println(wheat.price); 
+System.out.println(wheat.discount);	
+System.out.println(dates.price); 
+System.out.println(dates.discount);
+rice.buy(); // Method Calling Statement
+wheat.buy(); 
+dates.buy(); 
+}
+public void buy()
+{
+System.out.println("I am buying for " + price); 
+System.out.println(discount); 
+}
+
+}
